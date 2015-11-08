afc4c984d36c60485aa5585d998d1006d9962416
Marcial <marcial.lopez-ferrada@polymtl.ca>
2015-11-08 13:06:25 -0500

    corrction gitignore + less noise

--- TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
+++ TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
@@ -21,37 +21,45 @@ int sinoscope_image_openmp(sinoscope_t *ptr)
         return -1;
 
     sinoscope_t sino = *ptr;
-    int x, y, index, taylor;
-    struct rgb c;
-    float val, px, py;
+    /*int x, y, index, taylor;*/
+    int x, y;
+    int s_width = ptr->width;
+    int s_height = ptr->height;
+    /*struct rgb c;*/
+
     unsigned char* b = sino.buf;
 
-    /*printf("width: %d\n", sino.width);*/
-    /*printf("height: %d\n", sino.height);*/
-    /*printf("taylor: %d\n", sino.taylor);*/
-    /*printf("time: %f\n", sino.time);*/
 
-    #pragma omp parallel for private(x, y, c)
-    for(x = 1; x < sino.width; ++x){
-        for(y = 1; y < sino.height; ++y) {
+    #pragma omp parallel for private(x, y)
+    for(x = 1; x < s_width; ++x){
+        for(y = 1; y < s_height; ++y) {
+            struct rgb c;
+            int index, taylor;
+            float val, px, py;
+            int s_taylor = ptr->taylor;
+            float s_time = ptr->time;
+            float s_phase0 = ptr->phase0;
+            float s_phase1 = ptr->phase1;
+            int s_interval = ptr->interval;
+            float s_interval_inv = ptr->interval_inv;
             px = sino.dx * y - 2 * M_PI;
             py = sino.dy * x - 2 * M_PI;
             val = 0.0f;
-            for (taylor = 1; taylor <= sino.taylor; taylor += 2) {
-                val += sin(px * taylor * sino.phase1 + sino.time) / taylor + cos(py * taylor * sino.phase0) / taylor;
+            for (taylor = 1; taylor <= s_taylor; taylor += 2) {
+                val += sin(px * taylor * s_phase1 + s_time) / taylor + cos(py * taylor * s_phase0) / taylor;
             }
             val = (atan(1.0 * val) - atan(-1.0 * val)) / (M_PI);
             val = (val + 1) * 100;
-            value_color(&c, val, sino.interval, sino.interval_inv);
-            index = (y * 3) + (x * 3) * sino.width;
+            value_color(&c, val, s_interval, s_interval_inv);
+            index = (y * 3) + (x * 3) * s_width;
             b[index + 0] = c.r;
             b[index + 1] = c.g;
             b[index + 2] = c.b;
             /*if(x == 1 && y == 1){*/
-                /*printf("here\n");*/
-                /*printf("index = %d\t\t b[%d] = %u\n", index, index + 0, c.r);*/
-                /*printf("index = %d\t\t b[%d] = %u\n", index, index + 1, c.g);*/
-                /*printf("index = %d\t\t b[%d] = %u\n", index, index + 2, c.b);*/
+            /*printf("here\n");*/
+            /*printf("index = %d\t\t b[%d] = %u\n", index, index + 0, c.r);*/
+            /*printf("index = %d\t\t b[%d] = %u\n", index, index + 1, c.g);*/
+            /*printf("index = %d\t\t b[%d] = %u\n", index, index + 2, c.b);*/
             /*}*/
         }
     }
