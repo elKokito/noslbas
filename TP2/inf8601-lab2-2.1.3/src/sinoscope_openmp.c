6f8ae7f784cce574de16cf3703da5f61c80053d6
Francois Trudel <francoistrudel13@gmail.com>
2015-11-10 18:26:47 -0500

    Retrait des variables private de la structure, autant sinon moins de bruit... cest vraiment le critical la clef

--- TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
+++ TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
@@ -20,36 +20,23 @@ int sinoscope_image_openmp(sinoscope_t *ptr)
     if (ptr == NULL)
         return -1;
 
-    sinoscope_t sino = *ptr;
-    /*int x, y, index, taylor;*/
-    int x, y;
-    int s_width = ptr->width;
-    int s_height = ptr->height;
-    /*struct rgb c;*/
-
-    unsigned char* b = sino.buf;
 
     struct rgb c;
-    int index, taylor;
-    float val, px, py, s_dx, s_dy;
-    int s_taylor;
-    float s_time;
-    float s_phase0;
-    float s_phase1;
-    int s_interval;
-    float s_interval_inv;
+    int index, taylor, x, y;
+    float val, px, py;
+    const int s_width = ptr->width;
+    const int s_height = ptr->height;
+    const float s_dx = ptr->dx, s_dy= ptr->dy;
+    const int s_taylor = ptr->taylor;
+    const float s_time = ptr->time;
+    const float s_phase0 = ptr->phase0;
+    const float s_phase1 = ptr->phase1;
+    const int s_interval = ptr->interval;
+    const float s_interval_inv = ptr->interval_inv;
 
-    #pragma omp parallel for private(x, y, c, index, s_taylor, s_time, s_phase0, s_phase1, s_interval, s_interval_inv, val, px, py, s_dx, s_dy)
+    #pragma omp parallel for private(x, y, c, index, val, px, py, )
     for(x = 1; x < s_width; ++x){
         for(y = 1; y < s_height; ++y) {
-            s_taylor = ptr->taylor;
-            s_time = ptr->time;
-            s_phase0 = ptr->phase0;
-            s_phase1 = ptr->phase1;
-            s_interval = ptr->interval;
-            s_interval_inv = ptr->interval_inv;
-            s_dx = ptr->dx;
-            s_dy = ptr->dy;
             px = s_dx * y - 2 * M_PI;
             py = s_dy * x - 2 * M_PI;
             val = 0.0f;
@@ -63,16 +50,10 @@ int sinoscope_image_openmp(sinoscope_t *ptr)
             index = (y * 3) + (x * 3) * s_width;
             #pragma omp critical
             {
-                b[index + 0] = c.r;
-                b[index + 1] = c.g;
-                b[index + 2] = c.b;
+                ptr->buf[index + 0] = c.r;
+                ptr->buf[index + 1] = c.g;
+                ptr->buf[index + 2] = c.b;
             }
-            /*if(x == 1 && y == 1){*/
-            /*printf("here\n");*/
-            /*printf("index = %d\t\t b[%d] = %u\n", index, index + 0, c.r);*/
-            /*printf("index = %d\t\t b[%d] = %u\n", index, index + 1, c.g);*/
-            /*printf("index = %d\t\t b[%d] = %u\n", index, index + 2, c.b);*/
-            /*}*/
         }
     }
 
